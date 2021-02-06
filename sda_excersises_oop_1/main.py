3ea7db64e4fdd343b8657c73baec78352f8dc56f
Daniel <daniel.smolarek2012@gmail.com>
2021-02-06 11:49:20 +0100

    sloved ex

--- sda_excersises_oop_1/main.py
+++ sda_excersises_oop_1/main.py
@@ -1,11 +1,15 @@
 from sda_excersises_oop_1.Cat import Cat
 from sda_excersises_oop_1.Dog import Dog
+from sda_excersises_oop_2.employee import Employee
+from sda_excersises_oop_2.man import Manager
 
 
 def main():
-    lista = [Cat('cat1'),Dog('dog1')]
-    for animal in lista:
-        print(animal.make_sound())
+    manager = Manager('Daniel', 'Smolarek', 1991, 3000)
+    print(manager.who_am_i())
+
+
+
 
 
 if __name__ == '__main__':
